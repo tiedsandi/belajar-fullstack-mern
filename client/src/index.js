f505a5891a6796c530f2e19efa35c5a271b1b5b4
tiedsandi <fachransandi@gmail.com>
2023-08-28 09:02:50 +0700

    add background

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App";
-
-import { Provider } from "react-redux";
 import { store } from "./store/store";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
