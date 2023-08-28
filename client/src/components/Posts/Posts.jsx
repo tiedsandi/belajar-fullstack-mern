4030c2481950d372e9cd690e300858abdccc87d2
tiedsandi <fachransandi@gmail.com>
2023-08-28 08:55:49 +0700

    get data and show at post section

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core";
+import { useSelector } from "react-redux";
 
 import Post from "./post/Post";
 import useStyles from "./styles";
-import { useSelector } from "react-redux";
 
 const Posts = () => {
-	// const classes = useStyles();
+	const classes = useStyles();
 	const posts = useSelector((state) => state.posts);
 
-	console.log(posts);
-	return (
-		<>
-			<h1>Posts</h1>
-			<Post />
-			<Post />
-		</>
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid
+			className={classes.mainContainer}
+			container
+			alignItems="stretch"
+			spacing={3}
+		>
+			{posts.map((post) => (
+				<Grid key={post.id} item xs={12} sm={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
